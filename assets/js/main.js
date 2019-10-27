aaef802e313a6e303f7befd14212c9b4760d79ad
Hayden <haydenismet@gmail.com>
2019-10-27 22:09:50 +0000

    Added reload and replacement of search content. Fixed url link redirect to marvel.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ var searchSelect; //global variable to use further on in code
 ////////////////////////////////////////////////////////////
 
 ////////////////// CLICK EVENT - STORE RADIO BUTTON SELECTED - SHOW/HIDE YEARS  //////////////////
-$(".comic__search--list").on("click", "li", function () {
+$(".comic__search--list").on("click", "li", function() {
   searchSelect = $(this)
     .children()
     .val();
@@ -23,101 +23,105 @@ $(".comic__search--list").on("click", "li", function () {
 });
 ////////////////////////////////////////////////////////////
 
+////////////////// HIDE SEARCH HTML PRE-SUBMIT OF SEARCH DATA //////////////////
+
 $(".search")
   .children()
   .children(".primary__heading")
-  .hide(); // HIDE SEARCH TEMPLATE
+  .hide();
 
 $(".search")
   .find(".search__results--end")
-  .hide(); // HIDE SEARCH TEMPLATE
+  .hide();
 
-///////////////// SEARCH FUNCTION  ///////////////////
-$(".comic__search").on("submit", function (e) {
+////////////////////////////////////////////////////////////
+
+///////////////// SEARCH FUNCTION  ////////////////////////////
+
+$(".comic__search").on("submit", function(e) {
+  e.preventDefault();
+
+console.log($('.character__card__one').length);
+ $('.character__card__one').remove();
   
- if ($('a').is(':empty')) {
-   console.log('empty');
- } else {$('.test').empty(); }
+    var yearURL; //declare variables
+    var url;
+    var characterURL;
+    var yearChoice = $(".years__options").val(); // store year chosen from dropdown toggle
 
-  
-  e.preventDefault(); //prevent default page reload
-  console.log($(this));
-  var yearURL; //declare variables
-  var url;
-  var characterURL;
-  var yearChoice = $(".years__options").val(); // store year chosen from dropdown toggle
+    //URL for character selected to be used
+    characterURL =
+      "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=Spider-&orderBy=-name&limit=10" +
+      "&ts=" +
+      ts +
+      "&apikey=" +
+      publicKey +
+      "&hash=" +
+      hash;
 
-  //URL for character selected to be used
-  characterURL =
-    "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=Spider-&orderBy=-name&limit=10" +
-    "&ts=" +
-    ts +
-    "&apikey=" +
-    publicKey +
-    "&hash=" +
-    hash;
+    //URL for year selected to be used */
+    yearURL =
+      "https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&noVariants=true&dateRange=" +
+      yearChoice +
+      "-01-01%2C%20" +
+      yearChoice +
+      "-12-31&characters=1009610%2C1014873%2C1017603%2C1016181%2C1011347%2C1010727%2C1012200%2C1011197%2C1012295%2C1009609&limit=10" +
+      "&ts=" +
+      ts +
+      "&apikey=" +
+      publicKey +
+      "&hash=" +
+      hash;
 
-  //URL for year selected to be used */
-  yearURL =
-    "https://gateway.marvel.com:443/v1/public/comics?format=comic&formatType=comic&noVariants=true&dateRange=" +
-    yearChoice +
-    "-01-01%2C%20" +
-    yearChoice +
-    "-12-31&characters=1009610%2C1014873%2C1017603%2C1016181%2C1011347%2C1010727%2C1012200%2C1011197%2C1012295%2C1009609&limit=10" +
-    "&ts=" +
-    ts +
-    "&apikey=" +
-    publicKey +
-    "&hash=" +
-    hash;
-
-  switch (searchSelect) {
-    case "characters":
-      url = characterURL;
-      break;
-    case "years":
-      url = yearURL;
-      break;
-    default:
-      alert("Click the link first and FIX THIS!");
-  }
-
-  //Fetching api data
-  $.ajax({
-    type: "GET",
-    url: url,
-    success: function (responseSearch) {
-      $(".search__results__template__ul")
-        .children()
-        .remove(".loading");
-
-      $(".search__results--end").show();
-      $(".search")
-        .children()
-        .children(".primary__heading")
-        .show();
-      console.log(responseSearch);
-
-      $(".characters").remove();
-      $(".new__comics").remove();
-      $(".randomize").remove();
-      ////////////// SEARCH ///////////////
-      var source = $("#search__results__template__script").html();
-      var template = Handlebars.compile(source);
-      var searchData = responseSearch.data.results;
-      var displayData = template(searchData);
-      $(".search__results__template__ul").append(displayData);
-    },
-    beforeSend: function () {
-      $(".search__results__template__ul").prepend(
-        '<div class="loading"><img src="assets/img/ajax-loader.gif" alt="Loading" /></div>'
-      );
-    },
-
-    error: function () {
-      alert("There was an error getting marvel data.");
+    switch (searchSelect) {
+      case "characters":
+        url = characterURL;
+        break;
+      case "years":
+        url = yearURL;
+        break;
+      default:
+        alert("Click the link first and FIX THIS!");
     }
-  });
+
+    //Fetching api data
+    $.ajax({
+      type: "GET",
+      url: url,
+      success: function(responseSearch) {
+        $(".search__results__template__ul")
+          .children()
+          .remove(".loading");
+
+        $(".search__results--end").show();
+        $(".search")
+          .children()
+          .children(".primary__heading")
+          .show();
+        console.log(responseSearch);
+
+        $(".characters").remove();
+        $(".new__comics").remove();
+        $(".randomize").remove();
+
+        ////////////// SEARCH ///////////////
+        var source = $("#search__results__template__script").html();
+        var template = Handlebars.compile(source);
+        var searchData = responseSearch.data.results;
+        var displayData = template(searchData);
+        $(".search__results__template__ul").append(displayData);
+      },
+      beforeSend: function() {
+        $(".search__results__template__ul").prepend(
+          '<div class="loading"><img src="assets/img/ajax-loader.gif" alt="Loading" /></div>'
+        );
+      },
+
+      error: function() {
+        alert("There was an error getting marvel data.");
+      }
+    });
+  
 });
 
 ////////////////////////////////////////////////////////////
@@ -133,7 +137,7 @@ $.ajax({
     publicKey +
     "&hash=" +
     hash,
-  success: function (response) {
+  success: function(response) {
     console.log(response);
     $(".character__template__hb")
       .children()
@@ -154,13 +158,13 @@ $.ajax({
     $(".character__template__hb").append(displayData);
   },
 
-  beforeSend: function () {
+  beforeSend: function() {
     $(".character__template__hb").prepend(
       '<div class="loading"><img src="assets/img/ajax-loader.gif" alt="Loading" /></div>'
     );
   },
 
-  error: function () {
+  error: function() {
     alert("There was an error getting marvel data.");
   }
 });
@@ -181,7 +185,7 @@ function callComicDates(paramDate) {
       publicKey +
       "&hash=" +
       hash,
-    success: function (responseComics) {
+    success: function(responseComics) {
       $(".comic__listings")
         .children()
         .remove(".loading");
@@ -191,17 +195,15 @@ function callComicDates(paramDate) {
       var comicData = responseComics.data.results;
       var displayData = template(comicData);
       $(".comic__listings").append(displayData);
-      //console.log(comicData);
-      // console.log(response);
     },
 
-    beforeSend: function () {
+    beforeSend: function() {
       $(".comic__listings").prepend(
         '<div class="loading"><img src="assets/img/ajax-loader.gif" alt="Loading" /></div>'
       );
     },
 
-    error: function () {
+    error: function() {
       alert("There was an error getting marvel data.");
     }
   });
@@ -221,7 +223,7 @@ if ($("#week__new").is(":checked")) {
 ////////////////////////////////////////////////////////////
 
 ////////////////////// FOR WHEN TAB CLICKED ////////////////////////////
-$(".new__comics--ulist").on("click", function (e) {
+$(".new__comics--ulist").on("click", function(e) {
   if ($(e.target).is("#week__new")) {
     $(".character__card").remove();
     callComicDates("thisWeek");
@@ -260,7 +262,7 @@ function randomizeSpider() {
       publicKey +
       "&hash=" +
       hash,
-    success: function (todayIssue) {
+    success: function(todayIssue) {
       $(".randomize__ul")
         .children()
         .remove(".loading");
@@ -276,12 +278,12 @@ function randomizeSpider() {
 
       //  console.log(randomIssueIs);
     },
-    beforeSend: function () {
+    beforeSend: function() {
       $(".randomize__ul").prepend(
         '<div class="loading"><img src="assets/img/ajax-loader.gif" alt="Loading" /></div>'
       );
     },
-    error: function () {
+    error: function() {
       alert("There was an error getting marvel data.");
     }
   });
@@ -289,7 +291,7 @@ function randomizeSpider() {
 
 randomizeSpider();
 
-$(".randomize__again").on("submit", function (e) {
+$(".randomize__again").on("submit", function(e) {
   e.preventDefault();
 
   $(this)
